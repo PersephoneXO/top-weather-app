91db62e22db9e5b700e407f148cb3658ef015e9b
Persephone <its.persephone.xo@gmail.com>
2024-02-19 14:15:29 -0500

    attempt to fix resource not found issue

--- src/data-manager.js
+++ src/data-manager.js
@@ -181,7 +181,7 @@ export const dataManager=(function(){
         text.textContent="Enter your location to get started!";
         container.appendChild(text);
         let img=createDOM('img','landing-page-img');
-        img.setAttribute('src','../src/weather-landing-page-img.svg');
+        img.setAttribute('src','/src/weather-landing-page-img.svg');
         container.appendChild(img);
         let attribution=createDOM('p','img-attr');
         attribution.innerHTML='<a href="https://www.freepik.com/free-vector/earth-moon-concept-illustration_22881458.htm#query=globe&position=40&from_view=search&track=sph&uuid=40530c76-ce83-4458-ab7f-a6bab1e21b68">Image by storyset</a> on Freepik';
