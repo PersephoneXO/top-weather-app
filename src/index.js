9725c67742e4287be9f2010c1ac509a58502ee92
Persephone <its.persephone.xo@gmail.com>
2024-02-19 13:20:05 -0500

    error page finished

--- src/index.js
+++ src/index.js
@@ -17,8 +17,16 @@ document.addEventListener('DOMContentLoaded',()=>{
 //show data on page load
 searchBar.addEventListener('submit', async (e)=>{
     e.preventDefault();
+
+    try{
     let weatherData=await dataManager.getData();
     mainContainer.innerHTML="";
     let weatherContent=await dataManager.showData(weatherData);
     mainContainer.appendChild(weatherContent);
+    }catch(error){
+        mainContainer.innerHTML="";
+        mainContainer.appendChild(dataManager.createErrorPage());
+    }finally{
+
+    }
 });
